9543297fab8c201e166a28e55a9eafaf3602e6d8
HenriqueAgp <henriqueiris@live.com>
2023-05-02 17:20:28 -0300

    App de Mapa

---
+++ src/components/Banner/Index.jsx
@@ -0,0 +1,12 @@
+import styles from './Banner.module.scss';
+
+
+export default function Banner () {
+    return(
+        <div className={styles.banner}>
+            
+        <h1 className={styles.banner__title}>Equipment Searching App </h1>
+        </div>
+
+    )
+}
